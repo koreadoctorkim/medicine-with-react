18d6e7ff916a700e99a8462abe8f74f9ae812c94
rukidding918 <rukidding918@gmail.com>
2020-03-01 14:27:10 +0000

    Updates

---
+++ precache-manifest.dff979df118381b83bcb34b1fa6b71b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649f991e1f92dae7398960420077e9fe",
+    "url": "/medicine-with-react/index.html"
+  },
+  {
+    "revision": "8380a63cca6e2f6d3ca3",
+    "url": "/medicine-with-react/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "660c36e89dd86de6701e",
+    "url": "/medicine-with-react/static/js/2.08e26ed3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/medicine-with-react/static/js/2.08e26ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8380a63cca6e2f6d3ca3",
+    "url": "/medicine-with-react/static/js/main.97d587e1.chunk.js"
+  },
+  {
+    "revision": "1377f6a5df59abb65e6f",
+    "url": "/medicine-with-react/static/js/runtime-main.fde521be.js"
+  }
+]);
